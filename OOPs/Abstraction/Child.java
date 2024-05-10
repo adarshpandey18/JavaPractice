a7c7cf0069190ef0bc899108fe06dd70041264f7
Adarsh <itsadarshvijaypandey@gmail.com>
2024-05-10 12:50:56 +0530

    updated

--- OOPs/Abstraction/Child.java
+++
@@ -1,8 +0,0 @@
-package JavaPractice.OOPs.Abstraction;
-
-public class Child extends Parent{
-    @Override
-    public void greet(String name) {
-        System.out.println("Hey,"+ name+" how are you ?");
-    }
-}
--- OOPs/Abstraction/Parent.java
+++
@@ -1,8 +0,0 @@
-package JavaPractice.OOPs.Abstraction;
-
-public abstract class Parent {
-    abstract void greet(String name);
-    final void sayHello(String name) {
-        System.out.println("hello "+ name);
-    }
-}
