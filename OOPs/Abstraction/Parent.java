3d55c369bc8c5a084e0c556b8ae3dba6803e223f
Adarsh <itsadarshvijaypandey@gmail.com>
2024-05-10 12:40:26 +0530

    Added Abstraction

---
+++ OOPs/Abstraction/Parent.java
@@ -0,0 +1,8 @@
+package JavaPractice.OOPs.Abstraction;
+
+public abstract class Parent {
+    abstract void greet(String name);
+    final void sayHello(String name) {
+        System.out.println("hello "+ name);
+    }
+}
