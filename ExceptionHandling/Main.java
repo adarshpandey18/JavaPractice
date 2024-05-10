2be25141bf7cd6df1d9bea1fe68cbb9c5283793f
Adarsh <itsadarshvijaypandey@gmail.com>
2024-05-10 13:57:11 +0530

    Updated

--- ExceptionHandling/Main.java
+++ ExceptionHandling/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             int ans = 100 / 0;
+            System.out.println(ans);
         } catch (ArithmeticException e) {
             System.out.println(e);
         } finally {
