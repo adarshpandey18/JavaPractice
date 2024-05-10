57ee1058eb7b355b71a1a45d48cf7b7b837c9bd0
Adarsh <itsadarshvijaypandey@gmail.com>
2024-05-10 15:11:23 +0530

    Updated

--- Generic/GenericMethod/GenericMethod.java
+++ Generic/GenericMethod/GenericMethod.java
@@ -1,16 +1,13 @@
 package JavaPractice.Generic.GenericMethod;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GenericMethod {
     public static void main(String[] args) {
-        int[]list = {1,2,3};
-        add(list);
-        
+        Integer[] list = {1, 2, 3};
+        printArray(list); 
     }
+
     public static <T> void printArray(T[] list) {
-        for(T item : list) {
+        for (T item : list) {
             System.out.println(item);
         }
     }
